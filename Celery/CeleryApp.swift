c545b48f71c09bf98a748b92edec57c7ac6ae083
Skylar <skylarc55@gmail.com>
2023-07-12 19:58:46 -0400

    Initial Commit

---
+++ Celery/CeleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CeleryApp.swift
+//  Celery
+//
+//  Created by Skylar Clemens on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CeleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
